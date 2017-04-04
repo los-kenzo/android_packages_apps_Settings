eb8453bf5ed81a5d251aa2645d4a51599f6aa667
umang96 <umangleekha3@gmail.com>
2017-04-04 16:12:27 +0530

    Add statusbar section in customization
    Change-Id: I12abed238fda6e1a3789d1cf00bf13c4917addaf

--- src/com/android/settings/loskenzo/StatusBar.java
+++ src/com/android/settings/loskenzo/StatusBar.java
@@ -25,7 +25,6 @@ import android.content.ContentResolver;
 import android.app.WallpaperManager;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.hardware.fingerprint.FingerprintManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v14.preference.SwitchPreference;
@@ -38,29 +37,12 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class Misc extends SettingsPreferenceFragment implements
-         Preference.OnPreferenceChangeListener {
-
-	private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
-	private SwitchPreference mFingerprintVib;
-        private FingerprintManager mFingerprintManager;
+public class StatusBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.los_kenzo_misc);
-	ContentResolver resolver = getActivity().getContentResolver();
-	final PreferenceScreen prefScreen = getPreferenceScreen();
-
-	mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
-        if (!mFingerprintManager.isHardwareDetected()){
-            prefScreen.removePreference(mFingerprintVib);
-        } else {
-        mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
-        mFingerprintVib.setOnPreferenceChangeListener(this);
-        }
+        addPreferencesFromResource(R.xml.los_kenzo_statusbar);
 
     }
 
@@ -68,16 +50,5 @@ public class Misc extends SettingsPreferenceFragment implements
     protected int getMetricsCategory() {
         return MetricsEvent.DEVICEINFO;
     }
-
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-         ContentResolver resolver = getActivity().getContentResolver();
-	if (preference == mFingerprintVib) {
-             boolean value = (Boolean) newValue;
-             Settings.System.putInt(getActivity().getContentResolver(),
-                     Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
-             return true;
-          }
-          return false;
-      }
 
 }
